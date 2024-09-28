108c547e0866c9319110c433a2bcc4196d943b4a
SirHarryBit <72266160+SirHarryBit@users.noreply.github.com>
2024-09-28 07:37:23 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = 'http://arula-test-env.eba-xqi6qn8m.ap-south-1.elasticbeanstalk.com/api'; // Replace with your backend URL
+const API_URL = 'https://arula-test-env.eba-xqi6qn8m.ap-south-1.elasticbeanstalk.com/api'; // Replace with your backend URL
 
 const api = axios.create({
     baseURL: API_URL,
